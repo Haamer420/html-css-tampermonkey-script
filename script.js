3d8cca70991ea5439a1c95fd4d0f0c023c6af11d
Haamer <haamermarten925@gmail.com>
2024-03-12 12:53:54 +0200

    Create script.js
    alustus

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-12
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.tahvel.edu.ee/#/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
